a99c9a2a2463d348acd18cdf81db008bbfc24468
thatzcool <mycaptain0771@gmail.com>
2025-01-09 14:53:53 +0900

    day02_01

---
+++ src/day02/FourArithmetic.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class FourArithmetic {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 2;
+        int result = 0;   //처리 결과를 저장하는 변수
+
+        result = a + b;
+        System.out.printf("a + b = %d%n",result);
+        System.out.println("a + b = " + result);
+        result = a - b;
+        System.out.printf("a - b = %d%n",result);
+        result = a * b;
+        System.out.printf("a * b = %d%n",result);
+        result = a / b;
+        System.out.printf("a / b = %d%n",result);
+
+    }
+}
