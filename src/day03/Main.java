88fc8b9a5d5030b6486e8eb8583c308539f527cb
thatzcool <mycaptain0771@gmail.com>
2025-01-13 11:16:50 +0900

    day04_01

--- src/day03/Main.java
+++ src/day03/Main.java
@@ -2,6 +2,14 @@ package day03;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("첫 번째 프로그램입니다.");
     }
+
+    void adder(int num1,int num2){
+        int result = num1 + num2;
+        System.out.println("덧셈결과 : "+ result);
+        System.out.printf("%d + %d = %d",num1,num2,result);   // 10 + 20 = 30
+    }
+    void minus(){}
+    void multi(){}
+    void div(){}
 }
---
+++ src/day04/jungol/Main_9010.java
@@ -0,0 +1,18 @@
+package day04.jungol;
+
+public class Main_9010 {
+    public static void main(String[] args) {
+       //switching
+        int n1, n2 , temp;
+        n1 = 10;
+        n2 = 20;
+
+        temp = n1;
+        n1 = n2;
+        n2 = temp;
+
+        System.out.printf("%d %d",n1,n2);
+
+
+    }
+}
