7e8c98ad7b7644fc3affa10ddd1c615a985eed46
thatzcool <mycaptain0771@gmail.com>
2025-01-17 17:56:11 +0900

    day08_end

--- src/day08/array/MutiArrayEx.java
+++ src/day08/array/MutiArrayEx.java
@@ -7,18 +7,21 @@ public class MutiArrayEx {
                 {80,90,96},
                 {76,88}
         };
-        //배열의 길이  1차원 배열의 길이(반의 수)
-        System.out.println(scores.length + "scores[0]" + "scores[1]");
-        System.out.println(scores[0][0]);
-        //scores[1] 88출력
-        System.out.println(scores[1][1]);
+//        //배열의 길이  1차원 배열의 길이(반의 수)
+//        System.out.println(scores.length + "scores[0]" + "scores[1]");
+//        System.out.println(scores[0][0]);
+//        //scores[1] 88출력
+//        System.out.println(scores[1][1]);
+//
+//       for(int ban = 0 ; ban < scores.length; ban++){   //반의 수만큼 반복
+//
+//           for(int student = 0 ; student<scores[ban].length; student++ ){ //해당 반의 학생 수 만큼 반복
+//               System.out.println(scores[ban][student]);           //전체 scores 점수 출력
+//           }
+//       }
 
-       for(int ban = 0 ; ban < scores.length; ban++){   //반의 수만큼 반복
+       //for-each 문을 이용하여 2반의 학생들의 점수 합산하고 평균을 구하여 출력하세요 (평균은 소수점 첫째자리까지)
 
-           for(int student = 0 ; student<scores[ban].length; student++ ){ //해당 반의 학생 수 만큼 반복
-               System.out.println(scores[ban][student]);           //전체 scores 점수 출력 
-           }
-       }
 
 
     }
